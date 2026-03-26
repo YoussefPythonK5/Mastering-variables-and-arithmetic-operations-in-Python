c907f77becc61a08184bbda9d34867a7d41bb942
Youssef <ya103857@gmail.com>
2026-03-26 08:20:22 +0200

    Add files via upload

---
+++ Variables and arithmetic operations .py
@@ -0,0 +1,20 @@
+y = 10
+x = 5
+print (y+x)
+num1 = 10
+num2 = 5
+num3 = 6
+print (num1+num2*num3)
+print ("*" * 25)
+num4 = 60
+num5 = 23
+num6 = 5
+num7 = 10
+print (num4-num5+num6*num7)
+print ("*" * 30)
+print (4560-2537)
+print (2640-616)
+print (3124-1099)
+print ("*" * 10)
+print (3642-1616)
+print ("Happy new year 2026")
